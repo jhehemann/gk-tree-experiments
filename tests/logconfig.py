9ec895086d6cc85296487fa79e5f22c012e7790f
Jannik Hehemann <jannik@hehemann-melle.de>
2025-06-07 23:09:38 +0200

    test: add logging for tests

---
+++ tests/logconfig.py
@@ -0,0 +1,16 @@
+# logconfig.py
+import logging
+
+logger = logging.getLogger("unittest")
+# Entferne alle vorhandenen Handler, damit es keine Konflikte gibt
+# while logger.hasHandlers():
+#     logger.removeHandler(logger.handlers[0])
+for h in logger.handlers[:]:
+    logger.removeHandler(h)
+
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
+logger.propagate = False
